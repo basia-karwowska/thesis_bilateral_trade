caba41c2694f567d7d894b2c981a33d045e75362
basia-karwowska <108468378+basia-karwowska@users.noreply.github.com>
2024-10-27 12:59:56 +0100

    Delete profit.py

--- profit.py
+++
@@ -1,8 +0,0 @@
-'''
-A function to compute profit from the trade defined as:
-(p_b-p_s)*1(b>=p_b)*1(s<=p_s)
-'''
-def profit(buyer_price, seller_price, buyer_valuation, seller_valuation):
-    if buyer_valuation >= buyer_price and seller_valuation <= seller_price:
-        return buyer_price - seller_price
-    return 0
